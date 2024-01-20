3a14d8c30bc98fad02c4e3ce0f30b6ade0c2cd41
Bryan de Bourbon <bryandebourbon@gmail.com>
2024-01-19 22:32:35 -0500

    Initial Commit

---
+++ OntarioRail/OntarioRailApp.swift
@@ -0,0 +1,17 @@
+//
+//  OntarioRailApp.swift
+//  OntarioRail
+//
+//  Created by Bryan de Bourbon on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct OntarioRailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
